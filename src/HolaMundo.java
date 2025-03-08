bed31340ada20a9a4a3a412431d7241f5875bdab
titiBFG <santinosemec@gmail.com>
2025-03-07 21:42:07 -0300

    Primer commit

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+        if (args.length == 2) {
+            System.out.println("Hola, " + args[1]);
+        }
+        for (int i = 0 ; i < args.length ; ++i) {
+            System.out.println("Hola, " + args[i]);
+        }
+    }
 }

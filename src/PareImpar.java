cfcf234b53a9f20d8cd8330c1c1e429882edc768
titiBFG <santinosemec@gmail.com>
2025-03-10 22:24:30 -0300

    Segundo commit

--- src/PareImpar.java
+++ src/PareImpar.java
@@ -1,7 +1,7 @@
 public class PareImpar {
     public static void main(String[] args) {
         if (args.length%2 == 0) {
-            for(int i = 0; i < args.length; ++i) {
+            for (int i = 0; i < args.length; ++i) {
                 System.out.println("Hola, " + args[i]);
             }
         } else {
